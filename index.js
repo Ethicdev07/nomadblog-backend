6f634b9bd542452057daaa32fa5f310031fb8d93
Ethicdev07 <akinadejunior76@gmail.com>
2025-07-04 23:03:40 +0100

    files updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = require('./app');
 const connectDB = require('./config/db');
-// const logger = require('./utils/logger');
+
 
 const PORT = process.env.PORT || 8080;
 
